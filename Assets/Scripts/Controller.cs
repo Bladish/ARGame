a34232315533747e45401ce70c81e6a6540423e8
Jonathan Aronsson Olsson <jonte962010@hotmail.com>
2019-09-04 14:59:13 +0200

    added fresh script started code

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class Controller : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/GameWorld.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+using System;
+
+public class GameWorld : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject DetectedPlanePrefab;
+
+    void Start()
+    {
+        
+    }
+
+    public void UpdateGameWorld()
+    {
+        FindPlaceToSpawnPlayer();
+    }
+
+    private void FindPlaceToSpawnPlayer()
+    {
+        
+    }
+}
