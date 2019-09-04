ed65ceb9aa7bff7e1952743799da0d653728d049
HongKong <pajo0043@gmail.com>
2019-09-04 14:23:54 +0200

    Basic structure Update

---
+++ Assets/Scripts/UIView.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIView : UIController
+{
+    public int happyness;
+
+    void Start()
+    {
+        
+    }
+
+    public void HappyBarLoosingHappyness()
+    {
+
+    }
+}
