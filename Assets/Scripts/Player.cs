c3c97c69a6c3669762fde69e9506f3e1a66804cf
Unknown <jkm.svensson@gmail.com>
2019-09-05 09:29:12 +0200

    Player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,12 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{
-    public GameObject player;
-    
-    private Player()
-    {
-        if (player == null) Instantiate(player);
+{   
+    [SerializeField]
+    private GameObject player;
+    private Vector3 playerPosition;
+    private Quaternion playerRotation;
 
+    public static Player instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public Player(Vector3 anchorPosition, Quaternion anchorRotation)
+    {
+        playerPosition = anchorPosition;
+        playerRotation = anchorRotation;
+        Instantiate(player, playerPosition, playerRotation);
+    }
+
+    public void SetPlayerPosition(Vector3 position)
+    {
+        playerPosition = position;
+        player.transform.position = playerPosition;
+    }
+
+    public void SetPlayerRotation(Quaternion rotation)
+    {
+        playerRotation = rotation;
+        player.transform.rotation = playerRotation;
+    }
+
+    public Vector3 GetPlayerTransform()
+    {
+        return playerPosition;
+    }
+
+    public Quaternion GetPlayerRotation()
+    {
+        return playerRotation;
     }
 }
