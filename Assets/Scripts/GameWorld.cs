b4d2f06901a53fa318614c282bfdf6d2ab3eb302
Jonathan Aronsson Olsson <jonte962010@hotmail.com>
2019-09-05 15:38:51 +0200

    Started working on game world generator, modified visualizer on planes to have collision

--- Assets/Scripts/GameWorld.cs
+++ Assets/Scripts/GameWorld.cs
@@ -2,58 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 using GoogleARCore;
+using GoogleARCore.Examples.Common;
 using System;
 
+/// <author>
+/// Jonathan Aronsson Olsson
+/// ARCore DetectedPlaneGenerator Template
+/// </author>
+/// <summary>
+/// Visualize planes and save them in a list.
+/// </summary>
 public class GameWorld : MonoBehaviour
 {
-    TrackableHit hit;
-
+    public List<GameObject> currentPlanes = new List<GameObject>();
+    public GameObject planePrefab;
+    public List<DetectedPlane> planeList = new List<DetectedPlane>();
     void Start()
     {
-        
+        //planePrefab = Resources.Load("DetectedPlaneVisualizer") as GameObject;
     }
 
     public void UpdateGameWorld()
     {
+        CheckIfMotionIsTracking();
         FindPlaceToSpawnPlayer();
+    }
+
+    private void CheckIfMotionIsTracking()
+    {
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            return;
+        }
     }
 
     private void FindPlaceToSpawnPlayer()
     {
-        
-        if(Frame.Raycast(Screen.height / 2, Screen.width / 2, TrackableHitFlags.PlaneWithinPolygon, out hit))
+        Session.GetTrackables<DetectedPlane>(planeList, TrackableQueryFilter.New);
+
+        for (int i = 0; i < planeList.Count; i++)
         {
-            if (hit.Trackable is DetectedPlane)
-            {
-                Player.instance.CreatPlayer(hit.Pose.position, hit.Pose.rotation);
-            }
+            GameObject newPlane = Instantiate(planePrefab, Vector3.zero, Quaternion.identity);
+            newPlane.GetComponent<DetectedPlaneVisualizer>().Initialize(planeList[i]);
+
+            //currentPlanes.Add(newPlane);
+            //Debug.Log("Size of plane " + currentPlanes[0].transform.ToString());
         }
+        //if (Input.touchCount > 1 || (Touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        //{
+
+        //}
     }
 }
 
+//if(Frame.Raycast(Screen.height / 2, Screen.width / 2, TrackableHitFlags.PlaneWithinPolygon, out hit))
+//{
+//    if (hit.Trackable is DetectedPlane)
+//    {
 
-/*if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
-{
-    // Use hit pose and camera pose to check if hittest is from the
-    // back of the plane, if it is, no need to create the anchor.
-    if ((hit.Trackable is DetectedPlane) &&
-        Vector3.Dot(FirstPersonCamera.transform.position - hit.Pose.position,
-            hit.Pose.rotation* Vector3.up) < 0)
-    {
-        Debug.Log("Hit at back of the current DetectedPlane");
-    }
-    else
-    {
-        // Instantiate Andy model at the hit pose.
-        var andyObject = Instantiate(AndyAndroidPrefab, hit.Pose.position, hit.Pose.rotation);
-
-// Compensate for the hitPose rotation facing away from the raycast (i.e. camera).
-andyObject.transform.Rotate(0, k_ModelRotation, 0, Space.Self);
-
-        // Create an anchor to allow ARCore to track the hitpoint as understanding of the physical
-        // world evolves.
-        var anchor = hit.Trackable.CreateAnchor(hit.Pose);
-
-// Make Andy model a child of the anchor.
-andyObject.transform.parent = anchor.transform;
-*/
+//        //Instantiate(player, hit.Pose.position, hit.Pose.rotation);
+//    }
+//}
