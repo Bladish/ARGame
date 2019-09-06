5487c125bcbca4b4c2174a064fa79a825c882842
Ulrik <ulrik.bergstrom@gmail.com>
2019-09-06 13:53:55 +0200

    Added my scene and Eat script and Entertainment script

---
+++ Assets/Scripts/Eat.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Created by Ulrik
+/// </summary>
+
+public class Eat : Player
+{
+	[SerializeField] private int hungerPoints;
+
+	public Eat(int sethungerpoints)
+	{
+		hungerPoints = sethungerpoints;
+	}
+
+	public int HungerPointsChange
+	{
+		get => hungerPoints;
+		set => hungerPoints = value;
+	}
+}
+
+
+	//Do same as code above
+	//This is how to use properties
+
+	//public int HungerPoints { get; set; }
---
+++ Assets/Scripts/Entertainment.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Created by Ulrik
+/// </summary>
+
+public class Entertainment : Player
+{
+	[SerializeField] private int funPoints;
+
+	public Entertainment(int setFunPoints)
+	{
+		funPoints = setFunPoints;
+	}
+
+	public int FunPointsChange
+	{
+		get => funPoints;
+		set => funPoints = value;
+	}
+}
+
+
+	//private int eating = 100;
+	//private int whenEating
+	//Eat eat;
+
+	//private void Start()
+	//{
+	//	eat = new Eat(eating);
+	//	Test();
+	//}
+
+
+	//public void Test()
+	//{
+	//	Debug.Log(eat.HungerPointsChange);
+	//	eat.HungerPointsChange = eating + whenEating;
+	//	Debug.Log(eat.HungerPointsChange);
+	//}
