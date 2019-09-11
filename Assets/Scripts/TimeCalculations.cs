412b049dc3a62427a40f1eb1e87cb1fac5c6ace8
Unknown <jkm.svensson@gmail.com>
2019-09-11 14:11:28 +0200

    Still working on time

---
+++ Assets/Scripts/TimeCalculations.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimeCalculations : MonoBehaviour
+{
+    public TextMeshProUGUI time;
+    public TextMeshProUGUI timeSaved;
+    public TextMeshProUGUI timeDif;
+    public TextMeshProUGUI checkTimeRules;
+
+    private DateTime now;
+    private DateTime lastFeed;
+    private DateTime lastPlayed;
+    private TimeSpan timeDifference;
+    private DateTime timeRules;
+    private Double minutsToAdd = 1.00;
+    void Start()
+    {
+               
+    }
+
+    private void Update()
+    {
+        now = DateTime.Now;
+        time.text = now.ToString();
+    }
+
+    public void Eating()
+    {
+        lastFeed = now;
+        timeRules = now.AddMinutes(minutsToAdd);
+        timeSaved.text = lastFeed.ToString();
+    }
+
+    public void CalculateTimeDifference()
+    {
+        timeDifference = now - lastFeed;
+        timeDif.text = timeDifference.ToString();
+    }
+
+    public void CheckTimeCalculations()
+    {
+        Debug.Log(timeRules);
+        if (now > timeRules) checkTimeRules.text = "True";
+        else checkTimeRules.text = "False";
+    }
+
+}
