b07372c04c44b2a55104da18c6168f8c6750fffc
Jonathan Aronsson Olsson <jonte962010@hotmail.com>
2019-09-25 14:13:20 +0200

    Reset game update

--- Assets/Scripts/ResetGame.cs
+++ Assets/Scripts/ResetGame.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GoogleARCore;
 
 public class ResetGame : MonoBehaviour
 {
@@ -10,9 +11,13 @@ public class ResetGame : MonoBehaviour
     {
         gameController = GetComponent<GameController>();
     }
-    public void Reset()
+    public void Reset(GameObject player, GameObject visualAnchor, Anchor anchor)
     {
+        Destroy(player);
+        Destroy(visualAnchor);
+        Destroy(anchor);
         PlayerPrefs.DeleteAll();
         gameController.SetGameStart();
+        Debug.Log("RESTART");
     }
 }
