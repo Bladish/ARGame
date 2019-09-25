6bac7db9b5cf3cb66d24ccca4abc2b2fb5fb667c
Unknown <jkm.svensson@gmail.com>
2019-09-25 13:11:21 +0200

    Reset game

---
+++ Assets/Scripts/ResetGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetGame : MonoBehaviour
+{
+    GameController gameController;
+
+    void Start()
+    {
+        gameController = GetComponent<GameController>();
+    }
+    public void Reset()
+    {
+        PlayerPrefs.DeleteAll();
+        gameController.SetGameStart();
+    }
+}
